d99ea921c232e366a8e2991a907d42162f7f24bb
PandazNetwork <156095336+PandazNetwork@users.noreply.github.com>
2024-02-24 23:13:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', '1455678'))
-API_HASH = environ.get('API_HASH', 'a6b02f54dab5607e542eb216f28a1e6d')
-BOT_TOKEN = environ.get('BOT_TOKEN', '1643063196:AAElqqVI2O8oEqGWyXK35SLaFc5K0rrokig')
-ADMIN = environ.get('ADMIN', '1111214141') #seperate by id
-ACCOUNT_NAME = environ.get('ACCOUNT_NAME', 'NordVPN') 
-FORCESUB_CHANNEL = environ.get("FORCESUB_CHANNEL", "nordvpn_1") #username without @ 
-DB_URL = environ.get('DB_URL', 'mongodb+srv://whatsappbot:WbFWxnSrzNvXMzAA@whatsappbot.n058qik.mongodb.net/?retryWrites=true&w=majority')
+API_ID = int(environ.get('API_ID', ''))
+API_HASH = environ.get('API_HASH', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
+ADMIN = environ.get('ADMIN', '') #seperate by id
+ACCOUNT_NAME = environ.get('ACCOUNT_NAME', '') #for example: NordVPN
+FORCESUB_CHANNEL = environ.get("FORCESUB_CHANNEL", "") #username without @ 
+DB_URL = environ.get('DB_URL', '')
 user_combos_count = {} #Leave Black
-DAILY_LIMITS = environ.get("DAILY_LIMIT", "5")
+DAILY_LIMITS = environ.get("DAILY_LIMIT", "5") #default is 3
 
 #https://github.com/XSmitX/Telegram-Account-Generator-Bot
